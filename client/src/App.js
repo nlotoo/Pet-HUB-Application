d8c2e9181f96412dc5417740d3d43f065eccc611
Yordan Nikolov <jnikolow93@gmail.com>
2022-05-06 22:27:30 +0100

    setup antd

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+// import 'antd/dist/antd.css
+import 'antd/dist/antd.min.css'
+import { DatePicker } from 'antd';
+
+
+
+function App() {
+
+
+  return (
+
+    <DatePicker />
+
+
+
+  );
+}
+
+export default App;
