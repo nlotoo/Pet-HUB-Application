3dc7b7366a3b7099e891a2dc4a055c2f8df8e529
Yordan Nikolov <jnikolow93@gmail.com>
2022-05-26 10:53:40 +0100

    update resposive model

--- client/src/Components/AboutUsPage/AboutUsPage.js
+++ client/src/Components/AboutUsPage/AboutUsPage.js
@@ -2,24 +2,38 @@ import { useNavigate } from 'react-router-dom'
 import './aboutUs.css'
 
 import { useMediaQuery } from 'react-responsive'
+import { useEffect, useState } from 'react'
 
 
 
 const AboutUsPage = () => {
 
-    const navigate = useNavigate()
 
-    const isDesktopOrLaptop = useMediaQuery({ query: '(max-width: 1224px)' })
-    const isMobile = useMediaQuery({ query: '(max-width: 415px)' })
 
-    console.log(window.screen.height, window.screen.width)
+    // const isMobile = useMediaQuery({ query: '(max-width: 415px)' })
+    // const isTablet = useMediaQuery({ query: '(max-width: 821px)' })
+    // const isDesktopOrLaptop = useMediaQuery({ query: '(max-width: 1224px)' })
 
-    console.log(isDesktopOrLaptop, 'is 414')
-    console.log(isMobile, 'is 724px')
+    // let resolutionModel = () => {
+    //     if (isMobile) {
+    //         console.log('(max-width: 415px)')
+    //         return '-mobile'
+    //     } else if (isTablet) {
+    //         console.log('(max-width: 821px)')
+    //         return '-tablet'
+    //     } else if (isDesktopOrLaptop) {
+    //         console.log('(max-width: 1224px)')
+    //         return ''
+    //     }
+
+    // }
+
+    console.log(resolutionModel())
+    // console.log(isMobile, 'is 724px')
     return (
-        <div className={isMobile ? 'root-card-class-mobile' : 'root-card-class'}>
-            <div className={isMobile ? 'our-story-text-mobile' : 'our-story-text'}>
-                <p className={isMobile ? 'our-story-heading-mobile' : 'our-story-heading'}>Our Story</p>
+        <div className={`root-card-class${resolutionModel()}`}>
+            <div className={`our-story-text${resolutionModel()}`}>
+                <p className={`our-story-heading${resolutionModel()}`}>Our Story</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque soluta culpa adipisci ut error cupiditate quis explicabo harum rem eum tempore, architecto minima exercitationem tempora, deleniti laboriosam cum cumque numquam nemo obcaecati neque quo magni officia. Nobis cum nisi vel et aliquid illo magni animi atque sed fugiat deserunt necessitatibus architecto, libero labore voluptas velit omnis veniam? Ipsum, ducimus consequuntur.</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque soluta culpa adipisci ut error cupiditate quis explicabo harum rem eum tempore, architecto minima exercitationem tempora, deleniti laboriosam cum cumque numquam nemo obcaecati neque quo magni officia. Nobis cum nisi vel et aliquid illo magni animi atque sed fugiat deserunt necessitatibus architecto, libero labore voluptas velit omnis veniam? Ipsum, ducimus consequuntur.</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque soluta culpa adipisci ut error cupiditate quis explicabo harum rem eum tempore, architecto minima exercitationem tempora, deleniti laboriosam cum cumque numquam nemo obcaecati neque quo magni officia. Nobis cum nisi vel et aliquid illo magni animi atque sed fugiat deserunt necessitatibus architecto, libero labore voluptas velit omnis veniam? Ipsum, ducimus consequuntur.</p>
@@ -28,7 +42,7 @@ const AboutUsPage = () => {
 
             <div className='our-story-img' >
 
-                <img className='image-in-card' height={560} src={'https://advice.cdn.betterhelp.com/cro-people-who-love-animals-more-than-people-psychology-of-empathy-4.jpg'} />
+                <img className='image-in-card' height={460} src={'https://advice.cdn.betterhelp.com/cro-people-who-love-animals-more-than-people-psychology-of-empathy-4.jpg'} />
 
             </div>
 
---
+++ client/src/services/useResposiveDisplayTool.js
@@ -0,0 +1,27 @@
+
+
+
+
+import { useMediaQuery } from 'react-responsive'
+
+
+const useCustom = () => {
+
+    const isMobile = useMediaQuery({ query: '(max-width: 415px)' })
+    const isTablet = useMediaQuery({ query: '(max-width: 821px)' })
+    const isDesktopOrLaptop = useMediaQuery({ query: '(max-width: 1224px)' })
+
+    if (isMobile) {
+        console.log('(max-width: 415px)')
+        return '-mobile'
+    } else if (isTablet) {
+        console.log('(max-width: 821px)')
+        return '-tablet'
+    } else if (isDesktopOrLaptop) {
+        console.log('(max-width: 1224px)')
+        return ''
+    }
+
+}
+
+export default useCustom
