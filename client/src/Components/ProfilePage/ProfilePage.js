638b12ae8e384d285b2a530cd0b1abb62f04840b
Yordan Nikolov <jnikolow93@gmail.com>
2022-05-28 08:00:34 +0100

    user profile update

--- client/src/Components/ProfilePage/ProfilePage.js
+++ client/src/Components/ProfilePage/ProfilePage.js
@@ -3,40 +3,48 @@ import './profile-page.css'
 const ProfilePage = () => {
 
     return (
-        <div className="cards-container">
 
-            <div className="card card-one">
-                <header>
-                    <div className="avatar">
-                        <img src="https://randomuser.me/api/portraits/men/3.jpg" alt="Jhon Doe" />
-                    </div>
-                </header>
+        <div className='user-profile-heading'>
+            <h2>User Profile</h2>
+            <div className="cards-container">
+                <div className="card card-one">
+                    <header>
+                        <div className="avatar">
+                            <img src="https://randomuser.me/api/portraits/men/3.jpg" alt="Jhon Doe" />
+                        </div>
+                    </header>
 
-                <h3>Jhon Doe</h3>
-                <div className='user-info'>
-                    <div>
-                    <i class='fas fa-dog'></i>
-                        <span>Missy,Stuard,Barry</span>
+                    <h3>Jhon Doe</h3>
+                    <div className='user-info'>
+                        <div>
+                            <i class='fas fa-dog'></i>
+                            <span>&nbsp;&nbsp;Missy,Stuard,Barry</span>
+                        </div>
+
+                        <div>
+                            <i class='fas fa-mobile'></i>
+                            <span>&nbsp;&nbsp;+88888888</span>
+                        </div>
+
+                        <div>
+                            <i class='fas fa-location'></i>
+                            <span>&nbsp;&nbsp;Crediton</span>
+                        </div>
+                        <div>
+                            <i class='fas fa-envelope'></i>
+                            <span>&nbsp;&nbsp;example@abv.bg</span>
+                        </div>
+
                     </div>
-               
-                    <div>
-                    <i class='fas fa-mobile'></i>
-                        <span>   +88888888</span>
-                    </div>
-                    
-                    <div>
-                    <i class='fas fa-location'></i>
-                        <span>&&&&&Crediton</span>
-                    </div>
-                   
+                    <footer>
+
+                    </footer>
                 </div>
-                <footer>
 
-                </footer>
+
             </div>
-
-
         </div>
+
     )
 
 }
