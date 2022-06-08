eb3c297045170049e3e6738590a598f80bbbae34
Yordan Nikolov <jnikolow93@gmail.com>
2022-06-08 23:10:24 +0100

    integrate responsive navi menu

--- client/src/Components/core/NavigationBar/NavigationBar.js
+++ client/src/Components/core/NavigationBar/NavigationBar.js
@@ -9,40 +9,65 @@ import './NavigationBar.css'
 
 const NavigationBar = () => {
 
-    let isLoged = loginStorageCheker()
+    // let isLoged = loginStorageCheker()
 
 
 
-  
+
 
 
     const menuDrop = (e) => {
-
-        return 'tablet-menu-down'
-        
-
+        return '-tablet-menu-down'
     }
 
 
 
-    return (<div className='navbar-root' >
-        <header>
-            <nav className={`nav-bar${useResponsiveDesign()}`}>
-                {"-tablet" === useResponsiveDesign() ? <div className='icon'><button onClick={menuDrop} >&#9776;</button></div> : ''}
-                <ul className={`nav-bar-ul-list${useResponsiveDesign()}`}>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about-us">About</Link></li>
-                    <li><Link to="/contact-us">Contatct us</Link></li>
-                    {isLoged && <li><Link to="/user-profile">Profile</Link></li>}
-                    {isLoged && <li><Link to="/pets-catalog">Pets Catalog</Link></li>}
-                    {isLoged && <li><Link to="/create-new-pet">Add new pet</Link></li>}
-                    {!isLoged && <li><Link to="/sign-up">Register</Link></li>}
-                    {!isLoged && <li><Link to="/login">Login</Link></li>}
-                    {isLoged && <li><Link to="/logout">Logout</Link></li>}
-                </ul>
-            </nav>
-        </header>
-    </div >)
+    return (
+        <nav className='navigation-bar'>
+
+            <div className='logo'>
+                <h4>The Nav</h4>
+            </div>
+
+            <ul className='nav-links' >
+                <li>
+                    <Link to='#'>Home</Link>
+                </li>
+                <li>
+                    <Link to='#'>About</Link>
+                </li>
+                <li>
+                    <Link to='#'>Work</Link>
+                </li>
+            </ul>
+            <div className='burger'>
+                <div className='line1'></div>
+                <div className='line2'></div>
+                <div className='line3'></div>
+            </div>
+
+
+        </nav>
+    )
+
+    // return (<div className='navbar-root' >
+    //     <header>
+    //         <nav className={`nav-bar${useResponsiveDesign()}`}>
+    //             {"-tablet" === useResponsiveDesign() ? <div className='icon'><button onClick={menuDrop} >&#9776;</button></div> : ''}
+    //             <ul className={`nav-bar-ul-list${useResponsiveDesign()}`}>
+    //                 <li><Link to="/">Home</Link></li>
+    //                 <li><Link to="/about-us">About</Link></li>
+    //                 <li><Link to="/contact-us">Contatct us</Link></li>
+    //                 {isLoged && <li><Link to="/user-profile">Profile</Link></li>}
+    //                 {isLoged && <li><Link to="/pets-catalog">Pets Catalog</Link></li>}
+    //                 {isLoged && <li><Link to="/create-new-pet">Add new pet</Link></li>}
+    //                 {!isLoged && <li><Link to="/sign-up">Register</Link></li>}
+    //                 {!isLoged && <li><Link to="/login">Login</Link></li>}
+    //                 {isLoged && <li><Link to="/logout">Logout</Link></li>}
+    //             </ul>
+    //         </nav>
+    //     </header>
+    // </div >)
 
 };
 
