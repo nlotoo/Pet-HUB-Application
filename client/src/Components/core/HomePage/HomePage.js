b318fce55fbd7fe25fb543c3dd8ad1035b9eb602
Yordan Nikolov <jnikolow93@gmail.com>
2022-07-17 08:09:51 +0100

    home page update

--- client/src/Components/core/HomePage/HomePage.js
+++ client/src/Components/core/HomePage/HomePage.js
@@ -1,27 +1,30 @@
 import '../HomePage/homepage.css';
-import ImageSlider from '../../AdditionalComponents/SlideShowHomePage/SlideShow';
-
-
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
-
+    const navigate = useNavigate();
 
     return (
 
         <div className="wrapper">
             <div className="container">
+                <div className="inside-container">
+                    <section className='slider'>
+                        <img src='https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F34%2F2020%2F04%2F17%2Fwelsh-corgi-dog-garden-getty-0420.jpg' alt='travel-img' className='image' />
+
+
+                    </section>
+                </div>
                 <div className='home-page-text'>
                     <h2>You still can`t find your favorite dog breed! </h2>
                     <h3> Try for free with our online catalog for dogs !</h3>
 
-                   
+                    <button className='catalog-button' onClick={() => { navigate('/get-all-pets') }} >Our Catalog</button>
 
                 </div>
-                <div className="inside-container">
-                    <ImageSlider />
-                </div>
+
             </div>
-        </div>
+        </div >
     );
 };
 export default HomePage;
