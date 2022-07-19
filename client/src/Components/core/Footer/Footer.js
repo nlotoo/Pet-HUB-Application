b1b5007c7d5baf5666eb8ff2bbf2d03f57a78935
Yordan Nikolov <jnikolow93@gmail.com>
2022-07-19 08:07:19 +0100

    footer desing

--- client/src/Components/core/Footer/Footer.js
+++ client/src/Components/core/Footer/Footer.js
@@ -3,19 +3,25 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
 
     return (
-        <div >
+        
+            
+        <div className='footer-root' >
+          
             <span className="footer-content" >Copyright © 2022 Yordan Nikolov</span>
             <div className='footer-map-tree'>
                 <ul>
                     <li><Link to='/'></Link>Home</li>
                     <li><Link to='/'></Link>Our Catalog</li>
                     <li><Link to='/'></Link>About</li>
+                </ul>
+                <ul>
                     <li><Link to='/'></Link>My Profile</li>
                     <li><Link to='/'></Link>Your Catalog</li>
                     <li><Link to='/'></Link>Add new pet</li>
-                    <li><Link to='/'></Link>Login</li>
+                </ul>
+                <ul>
+                <li><Link to='/'></Link>Login</li>
                     <li><Link to='/'></Link>Register</li>
-
                 </ul>
             </div>
             <div className='social-media-button'>
