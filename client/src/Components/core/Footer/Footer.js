ba7b4d4e59a362e57897088c979825b67c6f847c
Yordan Nikolov <jnikolow93@gmail.com>
2022-07-20 21:17:39 +0100

    footer update

--- client/src/Components/core/Footer/Footer.js
+++ client/src/Components/core/Footer/Footer.js
@@ -1,33 +1,16 @@
 import './footer.css';
-import { Link } from 'react-router-dom';
 const Footer = () => {
 
     return (
-        
-            
+
         <div className='footer-root' >
-          
-            <span className="footer-content" >Copyright © 2022 Yordan Nikolov</span>
-            <div className='footer-map-tree'>
-                <ul>
-                    <li><Link to='/'></Link>Home</li>
-                    <li><Link to='/'></Link>Our Catalog</li>
-                    <li><Link to='/'></Link>About</li>
-                </ul>
-                <ul>
-                    <li><Link to='/'></Link>My Profile</li>
-                    <li><Link to='/'></Link>Your Catalog</li>
-                    <li><Link to='/'></Link>Add new pet</li>
-                </ul>
-                <ul>
-                <li><Link to='/'></Link>Login</li>
-                    <li><Link to='/'></Link>Register</li>
-                </ul>
-            </div>
-            <div className='social-media-button'>
-                <i className="fa-brands fa-instagram"></i>
-                <i className="fa-brands fa-facebook"></i>
-                <i className="fa-brands fa-twitter"></i>
+            <div>
+                <span className='coppy-right-text'>Copyright © 2022 Yordan Nikolov</span>
+                <div className='social-media-button'  >
+                        <i className="fa-brands fa-instagram"></i>
+                        <i className="fa-brands fa-facebook"></i>
+                        <i className="fa-brands fa-twitter"></i>
+                </div>
             </div>
         </div>
     );
